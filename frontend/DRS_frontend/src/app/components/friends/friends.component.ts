0be1178acd94141120d4a00aa2a5307b59439a5f
StefanCvjetkovic2 <cvjetkovic.pr34.2021@uns.ac.rs>
2024-11-23 03:44:24 +0100

    Implemented frontend functionality for the search.

--- frontend/DRS_frontend/src/app/components/friends/friends.component.ts
+++ frontend/DRS_frontend/src/app/components/friends/friends.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
+import {MatToolbar} from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-friends',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [SearchBarComponent,MatToolbar],
   templateUrl: './friends.component.html',
   styleUrl: './friends.component.css'
 })
