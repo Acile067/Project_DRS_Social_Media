7ea6af7d05827350d7435f30260c97263ef38ce3
Aleksandar <acile067@gmail.com>
2025-05-15 11:59:03 +0200

    secrets fix

---
+++ terraform/kv-secrets.tf
@@ -0,0 +1,14 @@
+data azurerm_key_vault main {
+  name                = "kv-socialmediadrs-mng"
+  resource_group_name = "rg-socialmediadrs-mng-eun"
+}
+
+data azurerm_key_vault_secret admin_login {
+  name         = "sql-admin"
+  key_vault_id = data.azurerm_key_vault.main.id
+}
+
+data azurerm_key_vault_secret admin_password {
+  name         = "sql-password"
+  key_vault_id = data.azurerm_key_vault.main.id
+}
